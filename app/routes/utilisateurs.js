ce143f11fb4a9d9920a17994296b8ab17176d1aa
maxence WATRIN <maxence.watr@gmail.com>
2019-06-07 08:31:01 +0200

    Update utilisateurs.js

--- app/routes/utilisateurs.js
+++ app/routes/utilisateurs.js
@@ -24,9 +24,27 @@ router.post('/', function(req, res, next) {
 
 
 /* update todo. */
-router.put('/:id', function(req, res, next) {
+router.post('/authentification/inscription/:identifiant/:motdepasse/:role', (req, res) => {
+    var identifiant = req.params.identifiant;
+    var motDePasse = req.params.motdepasse;
+    var role = req.params.role;
 
+
+    model.utilisateur.findAll({where: { identifiant: identifiant, motdepasse: motDePasse }})
+        .then( (result) => {
+            if(result.length > 0){
+                res.json({ 'response': { 'type': 'false', 'message': 'utilisateur existant'} });
+                console.log(res);
+            }else{
+                model.utilisateur.create({
+                    identifiant: identifiant,
+                    motdepasse: motDePasse,
+                    roleId: role,
+                }).then(res.json({'response': { 'type': 'true', 'message': 'Utilisateur créé' }}))
+                    .catch(errHandler);
+            }}).catch(errHandler);
 });
+
 
 
 /* GET todo listing. */
